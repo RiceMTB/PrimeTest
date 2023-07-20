d9e3f4a268220bb2b7b584177217e92e66af0b7f
RiceMTB <137529117+RiceMTB@users.noreply.github.com>
2023-07-20 15:12:32 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# PrimeTest
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vue-1versl)
---
+++ src/components/texbox.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div class="card flex justify-content-center">
+    <form @submit="onSubmit" class="flex flex-column gap-2">
+        <span class="p-float-label">
+            <Textarea id="value" v-model="value" :class="{ 'p-invalid': errorMessage }" rows="getrow()" cols= mycol aria-describedby="text-error" />
+            <label for="value">{{mylabel}} {{mycol}} col {{myrows}} row</label>
+        </span>
+    </form>
+    <Toast />
+</div>
+  </div>
+</template>
+<script>
+  export default{ 
+    props:['mycol', 'myrows', 'mylabel'],
+    methods:{ 
+      getrow(){ 
+        return this.myrows
+      }
+  }
+  }
+</script>
