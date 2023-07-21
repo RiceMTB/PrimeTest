a7c59b0de498fd93456881f8c4b0d10f967845bb
RiceMTB <137529117+RiceMTB@users.noreply.github.com>
2023-07-20 19:25:35 -0500

    Update texbox.vue

--- src/components/texbox.vue
+++ src/components/texbox.vue
@@ -3,7 +3,7 @@
     <div class="card flex justify-content-center">
     <form @submit="onSubmit" class="flex flex-column gap-2">
         <span class="p-float-label">
-            <Textarea id="value" v-model="value" :class="{ 'p-invalid': errorMessage }" rows="getrow()" cols= mycol aria-describedby="text-error" />
+            <Textarea id="value" v-model="value" :class="{ 'p-invalid': errorMessage }" rows="40" cols= mycol aria-describedby="text-error" />
             <label for="value">{{mylabel}} {{mycol}} col {{myrows}} row</label>
         </span>
     </form>
