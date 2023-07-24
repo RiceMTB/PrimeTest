dab550e5e475cacaa3b02b425176448a8ed4a76a
RiceMTB <137529117+RiceMTB@users.noreply.github.com>
2023-07-24 13:13:43 -0500

    2 files modified

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="app" >
  
     <primeHeader/>
     <textbox mylabel="Who can see this?"/>
@@ -45,6 +45,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 0px;
 }
 </style>
